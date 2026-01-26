20e66ca69338078fdcfacd0afa2f7b825ff50dd2
FoxfordOH <143639979+PhilosofoxDev@users.noreply.github.com>
2026-01-25 22:54:58 -0500

    Before react router experiments

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "tailwindcss";
 import Header from "./Header.jsx";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
 
 function App() {
   return (
     <div className="h-screen items-start align-top">
       <Header /> {/* Contains NavBar */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/portfolio" element={<PortfolioPage />} />
+      </Routes>
     </div>
   );
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -4,38 +4,35 @@ import { BrowserRouter as Router, Link, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 export default function Header() {
   return (
-    <div className="bg-amber-500/10 align-top items-start ">
-      <Router>
-        <header className="p-4 pb-6 w-screen">
-          <div className="grid grid-cols-2 gap-4 justify-between">
-            <div className="Logo flex items-center justify-start">
-              <h1 className="text-white text-xl font-bold">
-                Logo/Stylized Name
-              </h1>
-            </div>
-            <div className="NavigationBar flex justify-end align-top">
-              <nav className="p-4">
-                <ul className="grid-cols flex justify-end space-x-10">
-                  <li>
-                    <Link to="/" className="text-white text-2xl font-bold">
-                      Home
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/portfoliopage"
-                      className="text-white text-2xl font-bold"
-                    >
-                      Portfolio
-                    </Link>
-                  </li>
-                </ul>
-              </nav>
-            </div>
+    <div className="bg-stone-900 align-top items-start">
+      <header className="p-4 pb-6 w-screen">
+        <div className="grid grid-cols-2 gap-4 justify-between">
+          <div className="Logo flex items-center justify-start">
+            <h1 className="text-white text-xl font-bold pl-5">
+              Logo/Stylized Name
+            </h1>
           </div>
-        </header>
-        <HomePage />
-      </Router>
+          <div className="NavigationBar bg-white/50 flex justify-center align-top">
+            <nav className="p-4">
+              <ul className="grid-cols flex space-x-50">
+                <li>
+                  <Link to="/" className="text-white text-3xl font-bold">
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/portfoliopage"
+                    className="text-white text-3xl font-bold"
+                  >
+                    Portfolio
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
     </div>
   );
 }
