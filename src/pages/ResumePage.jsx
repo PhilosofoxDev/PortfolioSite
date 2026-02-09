d8c3ab9c908a1dfbbc3f97c22a1ba3f4620b6eb1
FoxfordOH <143639979+PhilosofoxDev@users.noreply.github.com>
2026-02-08 21:19:52 -0500

    Start Resume Page

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,10 +1,12 @@
 export default function ResumePage() {
   return (
-    <div className="overscroll-x-none flex flex-col items-center justify-center min-h-screen min-w-screen bg-stone-950">
-      <h1 className="text-5xl font-bold mb-6 text-orange-500">
-        What I've done
-      </h1>
-      <p className="text-lg text-gray-300 max-w-xl text-center">Resume here</p>
+    <div className="overscroll-x-none flex items-center pt-15 flex-col min-h-screen min-w-screen bg-stone-950">
+      <div className="flex flex-col ml-20 mt-8 mb-6 gap-15 items-start min-w-screen">
+        <h1 className="font-EmblemaOne border-b-3 text-5xl font-bold pb-2 pr-18 text-orange-500">
+          Experience
+        </h1>
+        <p className="text-2xl text-gray-300 max-w-xl text-center">Education</p>
+      </div>
     </div>
   );
 }
