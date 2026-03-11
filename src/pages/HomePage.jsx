4a4debc8c178697fae4e2fe14402b61194a357dc
FoxfordOH <143639979+PhilosofoxDev@users.noreply.github.com>
2026-03-10 22:23:57 -0700

    fixing homepage font test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ import "../cssFiles/HomePage.css";
 export default function HomePage() {
   return (
     <div className="noScroll flex flex-col justify-center bg-stone-900">
-      <div className="flex flex-col items-center self-center justify-center min-h-screen min-w-screen bg-stone-700">
+      <div className="flex flex-col items-center self-center justify-center min-h-screen min-w-screen bg-stone-500">
         {/* Video here in future ^^^ */}
         {/* <iframe
           width="105%"
@@ -21,7 +21,7 @@ export default function HomePage() {
         ></iframe> */}
         {/* remove zeros on 'outline' and 'bg' to add them back. 'bg' still needs some kind of opactiy */}
         <div className="flex flex-col sm:text-[1.5rem] md:text-[1.9rem] lg:text-[2.3rem] xl:text-5xl items-center gap-y-3 font-outline z-1 bg-stone-900/60 p-4 rounded-2xl pl-8 pr-8 outline-2 outline-orange-600">
-          <h1 className="flex font-bold align-bottom text-orange-500 text-shadow-stone-900 font-EmblemaOne">
+          <h1 className="flex font-bold align-bottom text-orange-500 text-shadow-stone-900">
             Nathan Lomnicky
           </h1>
 
