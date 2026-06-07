2de4c26eef7b6e8b53e22619cad71c0915e486ec
FoxfordOH <143639979+PhilosofoxDev@users.noreply.github.com>
2026-06-07 10:21:25 -0500

    beginning website redesign. Adding new colors

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ import DarkLogo from "./assets/images/NLDark.png";
 import StoneDark from "./assets/images/NLStone.png";
 export default function Header() {
   return (
-    <div className="bg-stone-900 align-top items-start overscroll-x-none w-screen border-b-3 border-orange-600">
+    <div className="bg-sageGreen align-top items-start overscroll-x-none w-screen">
       <header className="p-4 pb-6 w-screen">
         <div className="flex flex-col gap-4 items-center">
           {/* <div className="Logo flex items-center justify-start w-screen">
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,8 +5,8 @@ import "../cssFiles/HomePage.css";
 
 export default function HomePage() {
   return (
-    <div className="noScroll flex flex-col justify-center bg-stone-900">
-      <div className="flex flex-col items-center self-center justify-center min-h-screen min-w-screen bg-stone-500">
+    <div className="noScroll flex flex-col justify-center bg-deepTeal items-center w-screen h-screen">
+      <div className="flex flex-col items-center self-center justify-center min-h-screen min-w-screen bg-desertSand">
         {/* Video here in future ^^^ */}
         {/* <iframe
           width="105%"
@@ -20,7 +20,7 @@ export default function HomePage() {
           className="demoReel flex absolute self-center"
         ></iframe> */}
         {/* remove zeros on 'outline' and 'bg' to add them back. 'bg' still needs some kind of opactiy */}
-        <div className="flex flex-col sm:text-[1.5rem] md:text-[1.9rem] lg:text-[2.3rem] xl:text-5xl items-center gap-y-3 font-outline z-1 bg-stone-900/60 p-4 rounded-2xl pl-8 pr-8 outline-2 outline-orange-600">
+        {/* <div className="flex flex-col sm:text-[1.5rem] md:text-[1.9rem] lg:text-[2.3rem] xl:text-5xl items-center gap-y-3 font-outline z-1 bg-stone-900/60 p-4 rounded-2xl pl-8 pr-8 outline-2 outline-orange-600">
           <h1 className="flex font-bold align-bottom text-orange-500 text-shadow-stone-900">
             Nathan Lomnicky
           </h1>
@@ -33,7 +33,7 @@ export default function HomePage() {
           <h1 className="flex font-bold align-bottom text-orange-500 font-EmblemaOne">
             Gameplay and Systems Developer
           </h1>
-        </div>
+        </div> */}
       </div>
     </div>
   );
